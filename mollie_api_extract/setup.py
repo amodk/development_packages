3877b603d6bb2e234041189fe697cb60045810c8
Amod <amodk@outlook.com>
2020-12-08 22:50:30 +0530

    setting gitignore

--- mollie_api_extract/setup.py
+++ mollie_api_extract/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mollie-api-extract", # Replace with your own username
-    version="1.0.1",
+    version="1.0.2",
     author="Amod Kumar",
     author_email="amodk@outlook.com",
     description="Fetch data from your Mollie account",
